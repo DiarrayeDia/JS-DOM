aab8c1b584373d59c1a94158f3f92d8d65137bfc
DiarrayeDia <diarraye.dia@gmail.com>
2023-01-09 11:28:43 +0100

    exos dom

---
+++ 04_elementStyle.js
@@ -0,0 +1,5 @@
+function main() {
+    let element = document.getElementsByTagName("button");
+    return element[0].style.backgroundColor = "red";
+}
+console.log(main());
---
+++ 05_elementsStyle.js
@@ -0,0 +1,6 @@
+function main() {
+    let elements = document.getElementsByTagName("button");
+    return elements.style.backgroundColor = "red";
+    
+}
+console.log(main());
