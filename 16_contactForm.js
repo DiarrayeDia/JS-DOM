f2f4ce6e1312450f7cc5335d1248b32151ba9f9f
DiarrayeDia <diarraye.dia@gmail.com>
2023-01-11 16:50:35 +0100

    ex 16

---
+++ 16_contactForm.js
@@ -0,0 +1,9 @@
+let nom = document.getElementsByName("name")[0].value;
+let age = document.getElementsByName("age")[0].value;
+let birthdate = document.getElementsByName("birthdate")[0].value;
+let body = document.querySelector("body");
+
+const p = document.createElement("p");
+p.id="result";
+p.textContent = "name: " + nom + " age: " + age + " birthdate: " + birthdate;
+body.appendChild(p);
